bc13cf20ef62b8ceb6dca1155498aac109a7d54f
iv77777 <70826113+iv77777@users.noreply.github.com>
2022-03-26 00:14:18 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,9 +1,20 @@
+const fon = document.querySelector('.fon_js');
+const calendarMonthInner = document.querySelectorAll('.calendar__month-inner_js');
+
 document.addEventListener('click', (e) =>{
+  if (e.target.closest('.fon_js')){
+    fon.classList.remove('_active');
+    calendarMonthInner.forEach((item) =>{
+      item.classList.remove('_active');
+      item.style.cssText = '';
+    })
+  }
   if (e.target.closest('.calendar__month-inner_js')){
     const calendarMonthInner = e.target.closest('.calendar__month-inner_js');
     const calendarMonthWrapper = e.target.closest('.calendar__month-wrapper_js');
 
     calendarMonthInner.classList.toggle('_active');
+    fon.classList.toggle('_active');
 
     const MonthWrapperWidth = calendarMonthWrapper.offsetWidth;
     const windowInnerHeight = window.innerHeight;
@@ -16,7 +27,7 @@ document.addEventListener('click', (e) =>{
 
 
     let scale = MonthWrapperWidth / MonthInnerWidth;
-    // let scale = 2;
+
     if (scale > 3) {
       scale = 3;
     }
@@ -27,7 +38,7 @@ document.addEventListener('click', (e) =>{
       let ttY = (windowInnerHeight / 2) - (MonthInnerHeight / 2) - MonthInnerTop;    
 
       calendarMonthInner.style.cssText = `transform: scale(${scale}) 
-                                          translate(${ttX / scale}px, ${ttY / scale}px)`;
+                                          translate(${ttX / scale}px, ${ttY / scale}px);`;
     }else{
       calendarMonthInner.style.cssText = '';
     }
